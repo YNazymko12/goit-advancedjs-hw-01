1e1083d13cf88b28ae22a41eabd9c03d67669c0b
YNazymko12 <yulychka12@gmail.com>
2023-11-04 16:40:03 +0100

    Feat(JS) Add code in task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+
+function createItemsMarkup(){
+    return galleryItems.map(({preview, original, description}) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image lazyload"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`).join('')
+};
+
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryListEl.insertAdjacentHTML('beforeend', imagesMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionsData: 'alt',
+});
