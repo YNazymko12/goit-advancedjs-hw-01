d289daa9df3399c1fd1f0e1d4bfb9c7e3d1b7786
YNazymko12 <yulychka12@gmail.com>
2023-11-04 16:51:15 +0100

    Feat(JS) Add code in task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
 
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const VCT_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+function onPlay(evt) {
+    localStorage.setItem(VCT_KEY, evt.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(VCT_KEY)) || 0);
